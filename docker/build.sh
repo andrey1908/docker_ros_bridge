653325dbf9d1b78b1107a6562f83dfeffb0c1c74
andrey1908 <andrey.krishtopik@mail.ru>
2023-11-15 21:37:32 +0300

    docker: pass env variables RMW_IMPLEMENTATION, ROS_DOMAIN_ID and ROS_LOCALHOST_ONLY to docker image

--- docker/build.sh
+++ docker/build.sh
@@ -17,5 +17,8 @@ docker build . \
     --build-arg UID=$(id -g) \
     --build-arg GID=$(id -g) \
     --build-arg NUM_THREADS=${NUM_THREADS} \
+    --build-arg RMW_IMPLEMENTATION=${RMW_IMPLEMENTATION} \
+    --build-arg ROS_DOMAIN_ID=${ROS_DOMAIN_ID} \
+    --build-arg ROS_LOCALHOST_ONLY=${ROS_LOCALHOST_ONLY} \
     -f Dockerfile \
     -t ros_bridge:latest
