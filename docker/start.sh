b8b56d1e82da4476a9fba4dc59f932d70f210146
andrey1908 <andrey.krishtopik@mail.ru>
2023-11-17 01:56:45 +0300

    start.sh: use python with docker_helper to start docker container

--- docker/start.sh
+++ docker/start.sh
@@ -1,20 +1,6 @@
-#!/bin/bash
+#!/bin/env python3
 
-docker_dir=$(dirname $0)
+from docker_helper import RosDockerContainer
 
-docker run -it -d --rm \
-    --ipc host \
-    --gpus all \
-    --env="NVIDIA_DRIVER_CAPABILITIES=all" \
-    --env="DISPLAY=$DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --env="RMW_IMPLEMENTATION=$RMW_IMPLEMENTATION" \
-    --env="ROS_DOMAIN_ID=$ROS_DOMAIN_ID" \
-    --env="ROS_LOCALHOST_ONLY=$ROS_LOCALHOST_ONLY" \
-    --privileged \
-    --net "host" \
-    --name ros_bridge \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    -v /etc/timezone:/etc/timezone:ro \
-    -v /etc/localtime:/etc/localtime:ro \
-    ros_bridge:latest
+ros_bridge = RosDockerContainer("ros_bridge:latest", "ros_bridge")
+ros_bridge.create_containter()
