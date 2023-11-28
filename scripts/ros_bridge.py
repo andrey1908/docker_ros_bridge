5351bddd292bc59e56e02607847bbbb781d490d8
andrey1908 <andrey.krishtopik@mail.ru>
2023-11-28 12:12:28 +0300

    add services

--- scripts/ros_bridge.py
+++ scripts/ros_bridge.py
@@ -2,7 +2,6 @@ import os
 import os.path as osp
 import yaml
 from docker_helper import DockerMounts, RosDockerContainer
-from typing import List
 
 
 class RosBridgeMounts(DockerMounts):
@@ -45,17 +44,13 @@ class RosBridgeContainer(RosDockerContainer):
             "cd ~/colcon_ws && "
             "colcon build --cmake-force-configure")
 
-    def bridge_topics(self, topics: List[str], topic_types: List[str]=None):
-        if topic_types is None:
-            topic_types = [""] * len(topics)
-        assert len(topics) == len(topic_types)
-
-        for i, (topic, topic_type) in enumerate(zip(topics, topic_types)):
-            if topic == "/tf_static":
-                continue
-            if not topic_type:
-                topic_type = self._get_topic_type_str(topic)
-                topic_types[i] = topic_type
+    def run_bridge(self,
+            topics=tuple(), topic_types=None,
+            services_2_to_1=tuple(), services_2_to_1_types=None,
+            services_1_to_2=tuple(), services_1_to_2_types=None):
+        topics_param_str = self._prepare_topics(topics, topic_types)
+        services_2_to_1_param_str = self._prepare_services_2_to_1(services_2_to_1, services_2_to_1_types)
+        services_1_to_2_param_str = self._prepare_services_1_to_2(services_1_to_2, services_1_to_2_types)
 
         if "/tf_static" in topics:
             self.run_async(
@@ -64,14 +59,14 @@ class RosBridgeContainer(RosDockerContainer):
                 "ros2 run ros1_tf_static_bridge tf_static_bridge",
                 session="tf_static_bridge")
 
-        topics_param = self._get_topics_param(topics, topic_types)
-        topics_param_str = yaml.dump(topics_param, default_flow_style=True).rstrip()
         self.run(
             f"source ~/ros1_msgs_ws/devel/setup.bash && "
             f"source ~/ros2_msgs_ws/install/setup.bash && "
             f"cd ~/colcon_ws && "
             f"source install/setup.bash && "
             f"rosparam set /topics '{topics_param_str}' && "
+            f"rosparam set /services_2_to_1 '{services_2_to_1_param_str}' && "
+            f"rosparam set /services_1_to_2 '{services_1_to_2_param_str}' && "
             f"ros2 run ros1_bridge parameter_bridge")
 
         if "/tf_static" in topics:
@@ -84,6 +79,59 @@ class RosBridgeContainer(RosDockerContainer):
             "cd ~/colcon_ws && "
             "source install/setup.bash && "
             "ros2 run ros1_bridge dynamic_bridge --print-pairs")
+        
+    def _prepare_topics(self, topics, topic_types):
+        if topic_types is None:
+            topic_types = [""] * len(topics)
+        assert len(topics) == len(topic_types)
+
+        for i, (topic, topic_type) in enumerate(zip(topics, topic_types)):
+            if topic == "/tf_static":
+                continue
+            if not topic_type:
+                topic_type = self._get_topic_type_str(topic)
+                if topic_type is None:
+                    raise RuntimeError(f"Could not find topic {topic}.")
+                topic_types[i] = topic_type
+
+        topics_param = self._get_topics_param(topics, topic_types)
+        topics_param_str = yaml.dump(topics_param, default_flow_style=True).rstrip()
+
+        return topics_param_str
+    
+    def _prepare_services_2_to_1(self, services_2_to_1, services_2_to_1_types):
+        if services_2_to_1_types is None:
+            services_2_to_1_types = [""] * len(services_2_to_1)
+        assert len(services_2_to_1) == len(services_2_to_1_types)
+
+        for i, (service, service_type) in enumerate(zip(services_2_to_1, services_2_to_1_types)):
+            if not service_type:
+                service_type = self._get_ros1_service_type_str(service)
+                if service_type is None:
+                    raise RuntimeError(f"Could not find ROS1 service {service}.")
+                services_2_to_1_types[i] = service_type
+
+        services_2_to_1_param = self._get_services_2_to_1_param(services_2_to_1, services_2_to_1_types)
+        services_2_to_1_param_str = yaml.dump(services_2_to_1_param, default_flow_style=True).rstrip()
+
+        return services_2_to_1_param_str
+    
+    def _prepare_services_1_to_2(self, services_1_to_2, services_1_to_2_types):
+        if services_1_to_2_types is None:
+            services_1_to_2_types = [""] * len(services_1_to_2)
+        assert len(services_1_to_2) == len(services_1_to_2_types)
+
+        for i, (service, service_type) in enumerate(zip(services_1_to_2, services_1_to_2_types)):
+            if not service_type:
+                service_type = self._get_ros2_service_type_str(service)
+                if service_type is None:
+                    raise RuntimeError(f"Could not find ROS2 service {service}.")
+                services_1_to_2_types[i] = service_type
+
+        services_1_to_2_param = self._get_services_1_to_2_param(services_1_to_2, services_1_to_2_types)
+        services_1_to_2_param_str = yaml.dump(services_1_to_2_param, default_flow_style=True).rstrip()
+
+        return services_1_to_2_param_str
 
     def _get_ros1_topic_type_str(self, topic: str):
         command = \
@@ -95,8 +143,7 @@ class RosBridgeContainer(RosDockerContainer):
 
         output = res.stdout
         line = next((line for line in output.split('\n') if line.startswith("Type: ")), None)
-        if line is None:
-            return None
+        assert line is not None, "This should not happen"
         topic_type_str = line.rstrip()[len("Type: "):]
 
         # convert ROS1 topic type name to ROS2
@@ -113,8 +160,7 @@ class RosBridgeContainer(RosDockerContainer):
 
         output = res.stdout
         line = next((line for line in output.split('\n') if line.startswith("Type: ")), None)
-        if line is None:
-            return None
+        assert line is not None, "This should not happen"
         topic_type_str = line.rstrip()[len("Type: "):]
         return topic_type_str
 
@@ -122,8 +168,6 @@ class RosBridgeContainer(RosDockerContainer):
         ros1_topic_type_str = self._get_ros1_topic_type_str(topic)
         ros2_topic_type_str = self._get_ros2_topic_type_str(topic)
 
-        if ros1_topic_type_str is None and ros2_topic_type_str is None:
-            raise RuntimeError(f"Cound not find topic {topic}.")
         if ros1_topic_type_str is not None and ros2_topic_type_str is not None and \
                 ros1_topic_type_str != ros2_topic_type_str:
             raise RuntimeError(
@@ -132,7 +176,35 @@ class RosBridgeContainer(RosDockerContainer):
                 f"ROS2 - {ros2_topic_type_str}")
 
         return ros1_topic_type_str or ros2_topic_type_str
+
+    def _get_ros1_service_type_str(self, service: str):
+        command = \
+            f"source ~/ros1_msgs_ws/devel/setup.bash && " \
+            f"rosservice info {service}"
+        res = self.run(command, quiet=True)
+        if res.returncode != 0:
+            return None
+
+        output = res.stdout
+        line = next((line for line in output.split('\n') if line.startswith("Type: ")), None)
+        assert line is not None, "This should not happen"
+        service_type_str = line.rstrip()[len("Type: "):]
+        return service_type_str
     
+    def _get_ros2_service_type_str(self, service: str):
+        command = \
+            f"source ~/ros2_msgs_ws/install/setup.bash && " \
+            f"ros2 service type {service}"
+        res = self.run(command, quiet=True)
+        if res.returncode != 0:
+            return None
+
+        output = res.stdout
+        lines = [line for line in output.split('\n') if len(line.rstrip()) != 0]
+        assert len(lines) > 0, "This should not happen"
+        service_type_str = lines[-1].rstrip()
+        return service_type_str
+
     def _get_topics_param(self, topics, topic_types):
         topics_param = list()
         for topic, topic_type in zip(topics, topic_types):
@@ -149,3 +221,21 @@ class RosBridgeContainer(RosDockerContainer):
             topic_param["queue_size"] = queue_size
             topics_param.append(topic_param)
         return topics_param
+
+    def _get_services_2_to_1_param(self, services_2_to_1, services_2_to_1_types):
+        services_2_to_1_param = list()
+        for service, service_type in zip(services_2_to_1, services_2_to_1_types):
+            service_param = dict()
+            service_param["service"] = service
+            service_param["type"] = service_type
+            services_2_to_1_param.append(service_param)
+        return services_2_to_1_param
+
+    def _get_services_1_to_2_param(self, services_1_to_2, services_1_to_2_types):
+        services_1_to_2_param = list()
+        for service, service_type in zip(services_1_to_2, services_1_to_2_types):
+            service_param = dict()
+            service_param["service"] = service
+            service_param["type"] = service_type
+            services_1_to_2_param.append(service_param)
+        return services_1_to_2_param
--- scripts/run_ros_bridge.py
+++ scripts/run_ros_bridge.py
@@ -7,8 +7,14 @@ except ImportError:
 
 def build_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--topics', type=str, nargs='+')
-    parser.add_argument('--types', type=str, nargs='+')
+    parser.add_argument('--topics', type=str, nargs='+', default=tuple())
+    parser.add_argument('--topic-types', type=str, nargs='+')
+
+    parser.add_argument('--services-2-to-1', type=str, nargs='+', default=tuple(), help="allows ROS2 to call ROS1 services")
+    parser.add_argument('--services-2-to-1-types', type=str, nargs='+')
+
+    parser.add_argument('--services-1-to-2', type=str, nargs='+', default=tuple(), help="allows ROS1 to call ROS2 services")
+    parser.add_argument('--services-1-to-2-types', type=str, nargs='+')
 
     action = parser.add_mutually_exclusive_group()
     action.add_argument('--print-pairs', action='store_true')
@@ -35,4 +41,7 @@ if __name__ == "__main__":
     if args.topics is None:
         exit(0)
 
-    ros_bridge.bridge_topics(args.topics, topic_types=args.types)
+    ros_bridge.run_bridge(
+        topics=args.topics, topic_types=args.topic_types,
+        services_2_to_1=args.services_2_to_1, services_2_to_1_types=args.services_2_to_1_types,
+        services_1_to_2=args.services_1_to_2, services_1_to_2_types=args.services_1_to_2_types)
